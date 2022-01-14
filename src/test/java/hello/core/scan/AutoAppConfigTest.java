19fa5267975024fa68c9dbfccc6a7d9927ede5a1
emrhssla <emrhssla@gmail.com>
2022-01-14 21:52:43 +0900

    원복

--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -1,8 +1,10 @@
 package hello.core.scan;
 
 import hello.core.AutoAppConfig;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImp;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -16,5 +18,9 @@ public class AutoAppConfigTest {
         MemberService memberService = ac.getBean("memberServiceImpl", MemberService.class);
         // 기존 Appconfig의 memberService 와 같은 기능
         assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImp bean = ac.getBean(OrderServiceImp.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
     }
 }
