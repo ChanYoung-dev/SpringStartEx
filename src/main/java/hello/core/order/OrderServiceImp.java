06a08839cc0af694cc68e22b0ec640037b6204ac
emrhssla <emrhssla@gmail.com>
2022-01-14 21:43:13 +0900

    DI by Lomkom

--- src/main/java/hello/core/order/OrderServiceImp.java
+++ src/main/java/hello/core/order/OrderServiceImp.java
@@ -6,10 +6,12 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
+@RequiredArgsConstructor
 public class OrderServiceImp implements OrderService {
     //감독은 역할에만 신경쓸수있고 실행하는 책임만 진다
      private final MemberRepository memberRepository; //로미오 역할
@@ -19,12 +21,15 @@ public class OrderServiceImp implements OrderService {
     // 또는 private final MemberRepository memberRepository = new MemoryMemberRepository로 바꿀수있
     // 그래서 수정자 주입이나 다른 주입방식은 final를 사용하면 안된다
 
-
+    /*
+    @RequiredArgsConstructo로 인해 필요가 없어졌다
+    자동으로 생성하기때문이다
+    @Autowired // <- 생성자는 필요없다
     public OrderServiceImp(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-
+    */
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
